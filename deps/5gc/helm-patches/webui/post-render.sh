23132f6dbae98db056d94946c140b724279e3ecc
abel.ct <abelcastillot@gmail.com>
2025-08-11 12:00:58 -0400

    feat(5gc-deployment): enhance NF Helm values and add Kustomize integration
    - Improved NF configurations in Helm values file `sdcore-5g-values.yaml`
    - Integrated Kustomize installation into 5GC desploy playbook
    - Configured Kustomize to patch WebUI service for exposing port 5001

---
+++ deps/5gc/helm-patches/webui/post-render.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Obtener el directorio donde reside este script
+# Esto nos permite usar rutas relativas de forma segura
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# Navegar a ese directorio. Ahora todas las rutas relativas funcionarán como esperamos.
+cd "$SCRIPT_DIR"
+
+# Leer todo el YAML de Helm desde stdin y guardarlo en un archivo DENTRO de nuestro directorio.
+cat > all-helm-output.yaml
+
+# Ejecutar kustomize. Como ahora estamos en /tmp/helm-patches, la ruta relativa './webui' es correcta.
+kustomize build .
+
+# Limpiar el archivo temporal
+rm all-helm-output.yaml
