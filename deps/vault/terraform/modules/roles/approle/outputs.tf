06e37ed2d6a518e357c5fe9a738f9e47fb745b7b
PedroVhGit <pedrovh040110@gmail.com>
2025-12-05 16:06:24 -0500

    fix(roles): Correct output values for AppRole module to ensure accurate role information

--- deps/vault/terraform/modules/roles/approle/outputs.tf
+++ deps/vault/terraform/modules/roles/approle/outputs.tf
@@ -2,16 +2,16 @@
 
 output "approle_role_name" {
   description = "The name of the AppRole."
-  value       = vault_approle_auth_backend_role.app_role
+  value       = vault_approle_auth_backend_role.app_role.role_name
 }
 
 output "approle_role_id" {
   description = "The role ID of the AppRole."
-  value       = vault_approle_auth_backend_role.app_role
+  value       = vault_approle_auth_backend_role.app_role.role_name
   sensitive   = true
 }
 
 output "approle_backend" {
   description = "The backend of the AppRole."
-  value       = vault_approle_auth_backend_role.app_role
+  value       = vault_approle_auth_backend_role.app_role.role_name
 }
