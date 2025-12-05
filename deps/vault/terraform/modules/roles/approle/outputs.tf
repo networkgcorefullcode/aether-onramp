e5d3c91f091e6830e5d42af1c614076d571b5f8c
PedroVhGit <pedrovh040110@gmail.com>
2025-12-05 15:48:20 -0500

    feat(roles): Update outputs for AppRole module to enhance clarity

--- deps/vault/terraform/modules/roles/approle/outputs.tf
+++ deps/vault/terraform/modules/roles/approle/outputs.tf
@@ -2,16 +2,16 @@
 
 output "approle_role_name" {
   description = "The name of the AppRole."
-  value       = length(vault_approle_auth_backend_role.app_role) > 0 ? vault_approle_auth_backend_role.app_role[0].role_name : null
+  value       = vault_approle_auth_backend_role.app_role
 }
 
 output "approle_role_id" {
   description = "The role ID of the AppRole."
-  value       = length(vault_approle_auth_backend_role.app_role) > 0 ? vault_approle_auth_backend_role.app_role[0].role_id : null
+  value       = vault_approle_auth_backend_role.app_role
   sensitive   = true
 }
 
 output "approle_backend" {
   description = "The backend of the AppRole."
-  value       = length(vault_approle_auth_backend_role.app_role) > 0 ? vault_approle_auth_backend_role.app_role[0].backend : null
+  value       = vault_approle_auth_backend_role.app_role
 }
