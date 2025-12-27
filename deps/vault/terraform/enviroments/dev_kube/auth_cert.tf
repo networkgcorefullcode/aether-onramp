a9f0d7af2e2272103ab21d19304756c8079176d0
PedroVhGit <pedrovh040110@gmail.com>
2025-12-27 15:43:11 -0500

    remove innecesary auths

--- deps/vault/terraform/enviroments/dev_kube/auth_cert.tf
+++
@@ -1,9 +0,0 @@
-module "auth_backend_cert" {
-  source = "../../modules/auth_backend"
-
-  type              = "cert"
-  description       = "Development environment auth backend for cert"
-  path              = "auth-dev-cert"
-  default_lease_ttl = "3600s"
-  max_lease_ttl     = "7200s"
-}
--- deps/vault/terraform/enviroments/dev_kube/auth_jwt.tf
+++
@@ -1,9 +0,0 @@
-module "auth_backend_jwt" {
-  source = "../../modules/auth_backend"
-
-  type              = "jwt"
-  description       = "Development environment auth backend for jwt"
-  path              = "auth-dev-jwt"
-  default_lease_ttl = "3600s"
-  max_lease_ttl     = "7200s"
-}
